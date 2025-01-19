f64dda874f833301699bb06dc35bfc624c333e70
SergeyGB <sbilevskiy@gmail.com>
2025-01-19 21:55:03 +0300

    .

--- monets.py
+++ monets.py
@@ -1,9 +1,6 @@
 import time
 import uiautomator2 as u2
-import logging
 from datetime import datetime as dt
-
-
 
 ##########
 #0  x 1080
@@ -18,62 +15,93 @@ class  UI:
     def yy(self, y):
      return y
 
-    def pe(self):
-        for elem in self.d.xpath("//android.widget.TextView").all():
-            print("Text:", elem.text)
-            print("Attrib:", elem.attrib)
-            print("Position:", elem.center())
-
-    def wait(salf, sec=10):
-        time.sleep(sec)
-
-    def test_simple(self):
+    def Init(self):
         self.d = u2.connect()
         print(self.d.info)
         self.width, self.height = self.d.window_size()
 
+    def pe(self,xp="//android.widget.TextView"):
+        for elem in self.d.xpath(xp).all():
+            print("Text:", elem.text)
+            print("Attrib:", elem.attrib)
+            print("Position:", elem.center())
+
+    def ss(self, fn=""):
+        self.d.screenshot(dt.now().strftime("%Y-%m-%d_%H_%M") +  fn + ".jpg")
+
+    def xml(self, fn=""):
+        with open(dt.now().strftime("%Y-%m-%d_%H_%M") +  fn + ".xml", 'w') as f:
+           s = self.d.dump_hierarchy()
+           f.write(s)
+
+    def wait(salf, sec=10):
+        time.sleep(sec)
+
+    def GetAli(self):
         #self.d.app_stop_all()
         self.d.app_stop("ru.aliexpress.buyer")
         app = self.d.session("ru.aliexpress.buyer")
-        #self.wait(10)
-        #print( self.d.app_current() )
-
+        self.wait(2)
         if self.d(textContains='Профиль').exists(timeout=30) is True:
-           self.d.screenshot(dt.now().strftime("%Y-%m-%d_%H_%M")+"A.jpg")
+           self.ss("A1")
            self.d(textContains='Профиль').click()
            self.wait(2)
            if self.d(textContains='Чаты').exists(timeout=30) is True:
-              self.d.swipe(1000, 1500, 1000, 0)
+               self.ss("A2")
+               self.d.swipe(1000, 1500, 1000, 0)
+               self.wait(2)
            try:
+              self.ss("A3")
               self.d.xpath('//*[@resource-id="DisneylandBanner"]').click()
-              self.wait(5)
+              self.wait(2)
+              if self.d(textContains='День 1').exists(timeout=30) is True:
+                  self.ss("A4")
+                  if self.d(textContains='Монеты собраны!').exists(timeout=30) is False:
+                    self.d.click(self.d(textContains='День 1').center()[0],
+                                 self.d(textContains='День 1').center()[1] + 360)
+                    self.wait(5)
+                    self.ss("AX")
+                  else:
+                    self.ss("A0")
            except:
-              print("ex1")
+               self.d.app_stop("ru.aliexpress.buyer")
+               self.ss("AE1")
         else:
-           print("<Профиль> не найдена")
-           self.d.screenshot(dt.now().strftime("%Y-%m-%d_%H_%M")+"PNF.jpg")
-
+           self.ss("AE2")
         self.wait(3)
-
-        # self.d(textContains='+2').click()
-        if self.d(textContains='День 1').exists(timeout=30) is True:
-          self.d.click(self.d(textContains='День 1').center()[0], self.d(textContains='День 1').center()[1] + 360)
-          self.wait(2)
-          #ui.d.click(ui.d(textContains='День 2').center()[0], ui.d(textContains='День 2').center()[1] + 100)
-          #self.wait(2)
-          #ui.d.click(ui.d(textContains='День 3').center()[0], ui.d(textContains='День 3').center()[1] + 100)
-          #self.wait(2)
-          #ui.d.click(ui.d(textContains='День 4').center()[0], ui.d(textContains='День 4').center()[1] + 100)
-          #self.wait(2)
-          #ui.d.click(ui.d(textContains='День 5').center()[0], ui.d(textContains='День 5').center()[1] + 100)
-          #self.wait(2)
-          #ui.d.click(ui.d(textContains='День 6').center()[0], ui.d(textContains='День 6').center()[1] + 100)
-          #self.wait(10)
-        #self.d.app_stop_all()
         self.d.app_stop("ru.aliexpress.buyer")
 
+    def GetYan(self):
+
+        self.d.app_stop("ru.beru.android")
+        app = self.d.session("ru.beru.android")
+        self.wait(20)
+        self.xml("Y0")
+
+        if self.d(textContains='Пункт выдачи').exists(timeout=30) is True:
+           self.ss("Y1")
+           e=self.d(textContains='Пункт выдачи')
+           self.xml("Y1")
+           ui.d.click( e.center()[0]+500, e.center()[1] )
+           self.wait(2)
+           self.ss("Y2")
+           self.xml("Y2")
+           ui.d.click(860, 160)
+           self.wait(2)
+           self.ss("Y3")
+           self.xml("Y3")
+           ui.d.click(500, 1900)
+           self.wait(2)
+           self.ss("Y4")
+           self.xml("Y4")
+        else:
+           print("Пункт выдачи не найдено")
+           self.ss("YE")
+        self.d.app_stop("ru.beru.android")
+
 if __name__ == "__main__":
-#    logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
     ui = UI()
-    ui.test_simple()
+    ui.Init()
+    ui.GetAli()
+    ui.GetYan()
 
